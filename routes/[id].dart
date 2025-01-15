cc7f183a158c1e33ae9a1fff32c5ff049a2a3827
Sergey Savchuk <sleepingdog.dev@gmail.com>
2025-01-15 19:59:37 +0300

    update endpoint

--- routes/[id].dart
+++ routes/[id].dart
@@ -1,15 +1,15 @@
 import 'package:barcode/barcode.dart';
 import 'package:dart_frog/dart_frog.dart';
 
-Future<Response> onRequest(RequestContext context) async {
-  final body = context.request.uri.queryParameters['id'] ?? '';
+Future<Response> onRequest(RequestContext context, String id) async {
+  final body = id;
   final qr = Barcode.qrCode();
   final svg = qr.toSvg(body, width: 300, height: 300);
 
   return Response.bytes(
     body: svg.codeUnits,
     headers: {
-      'Content-Type': 'image/svg+xml',
+      'Content-Type': 'image/svg',
     },
   );
 }
